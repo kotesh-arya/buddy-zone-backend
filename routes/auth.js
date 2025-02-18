486f4375fe1fee436923e2ea5508951b9654703e
kotesh-arya <koteswarraomudila@gmail.com>
2025-02-19 01:21:32 +0530

    FIX: Allowing cookies to be stored on different domains

--- routes/auth.js
+++ routes/auth.js
@@ -72,7 +72,7 @@ router.post("/signup", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict", // CSRF protection
+      sameSite: "None", // CSRF protection
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
