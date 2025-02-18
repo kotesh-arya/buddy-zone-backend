96036a6efe4877c0cfea0b9bb31a06b66272254a
kotesh-arya <koteswarraomudila@gmail.com>
2025-02-19 01:26:52 +0530

    FIX: cookie storage on login also

--- routes/auth.js
+++ routes/auth.js
@@ -120,7 +120,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
